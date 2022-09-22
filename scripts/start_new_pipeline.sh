2fd6fe6507b56a4d2b2e39a8f9ce0e4677e2e909
mahammadism <mahammadism.s@gmail.com>
2022-09-23 01:25:42 +0530

    added new start_new_pipeline file

---
+++ scripts/start_new_pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+STATUS="$(aws codepipeline list-pipeline-executions --pipeline-name ismail --region ap-southeast-2 | grep '"status": "Succeeded"' | sort -u)"
+output=("status": "Succeeded",)
+if [[ "${STATUS}"="${OUTPUT}" ]]; then
+    aws codepipeline start-pipeline-execution --name nodejs-express-on-aws-ec2-1 --region ap-southeast-2
+else
+    echo " Pipeline got failed "
+    exit 1
+fi
